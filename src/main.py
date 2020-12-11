7bf3863a45b8a7f5c8ca900d225db92e9392ccf8
Pavel Bartsits <pavelbartsits@gmail.com>
2020-12-11 11:19:23 +0300

    check for instance of class on img that it is not the instance of the same class pandas table

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
 import os
-from collections import Counter
-from itertools import chain, combinations
-from collections import defaultdict
 import pandas as pd
 
 import supervisely_lib as sly
@@ -12,38 +9,59 @@ my_app = sly.AppService()
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
-DATASET_ID = os.environ.get('modal.state.slyDatasetId')
+DATASET_ID = int(os.environ.get('modal.state.slyDatasetId'))
 
-if DATASET_ID is not None:
-  DATASET_ID = int(DATASET_ID)
+def group_labels(labels):
+  d = {}
+
+  for l in labels:
+    k = l['classTitle']
+
+    if k in d:
+        d[k] = True
+    else:
+        d[k] = False
+
+  return d
 
 @my_app.callback("interactive_coexistence_matrix")
 @sly.timeit
 def interactive_coexistence_matrix(api: sly.Api, task_id, context, state, app_logger):
     classes = api.project.get_meta(PROJECT_ID)['classes']
 
-    if DATASET_ID is not None:
-        dataset_ids = [DATASET_ID]
-    else:
+    if PROJECT_ID is not None:
         datasets_list = api.dataset.get_list(PROJECT_ID)
         dataset_ids = [d.id for d in datasets_list]
+    else:
+        dataset_ids = [DATASET_ID]
 
-    coexist_table = {}
-
+    co_occurrence_table = {}
     for cls1 in classes:
-      for cls2 in classes:
-        coexist_table[f"{cls1['title']}-{cls2['title']}"] = []
+        for cls2 in classes:
+            co_occurrence_table[f"{cls1['title']}-{cls2['title']}"] = []
 
     for dataset_id in dataset_ids:
         anns = api.annotation.get_list(dataset_id)
-
         for ann_info in anns:
             image_id = ann_info.image_id
-            classes_on_img = set([label['classTitle'] for label in ann_info.annotation['objects']])
-
+            classes_on_img = group_labels(ann_info.annotation['objects'])
             for cls_name1 in classes_on_img:
                 for cls_name2 in classes_on_img:
-                    coexist_table[f"{cls_name1}-{cls_name2}"].append(image_id)
+                    if cls_name1 != cls_name2 or classes_on_img[cls_name1] == True:
+                        co_occurrence_table[f"{cls_name1}-{cls_name2}"].append(image_id)
+
+    pd_data = []
+    columns = ["name", *[cls["title"] for cls in classes]]
+    for cls1 in classes:
+        cur_row = [cls1["title"]]
+        for cls2 in classes:
+            imgs_cnt = len(co_occurrence_table[f'{cls1["title"]}-{cls2["title"]}'])
+            cur_row.append(imgs_cnt)
+        pd_data.append(cur_row)
+
+    df = pd.DataFrame(data=pd_data, columns=columns)
+    df.style.background_gradient(cmap='YlGn') \
+        .set_properties(**{'font-size': '20px'})
 
     my_app.stop()
 
