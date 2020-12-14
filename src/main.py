5a852a3f4a11089706905b3b3aec5e95e97c59b7
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-12-14 16:40:27 +0200

    table with colors

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import os
 import pandas as pd
 import seaborn as sns
+import matplotlib
 import itertools
 from collections import defaultdict
-
-# https://pandas.pydata.org/pandas-docs/stable/user_guide/style.html#Builtin-styles
-
 import supervisely_lib as sly
+
 
 my_app = sly.AppService()
 
@@ -16,6 +15,10 @@ PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 DATASET_ID = os.environ.get('modal.state.slyDatasetId', None)
 
 CELL_TO_IMAGES = None
+
+cmap = sns.light_palette("green", as_cmap=True)
+#https://stackoverflow.com/questions/25408393/getting-individual-colors-from-a-color-map-in-matplotlib
+#https://towardsdatascience.com/heatmap-basics-with-pythons-seaborn-fb92ea280a6c
 
 @my_app.callback("interactive_occurrence_matrix")
 @sly.timeit
@@ -38,6 +41,7 @@ def interactive_occurrence_matrix(api: sly.Api, task_id, context, state, app_log
         project = project._replace(items_count=api.project.get_images_count(project.id))
 
     fields = [
+        {"field": "data.started", "payload": True},
         {"field": "data.projectId", "payload": project.id},
         {"field": "data.projectName", "payload": project.name},
         {"field": "data.projectPreviewUrl", "payload": api.image.preview_url(project.reference_image_url, 100, 100)},
@@ -54,11 +58,9 @@ def interactive_occurrence_matrix(api: sly.Api, task_id, context, state, app_log
     for dataset_id in datasets_ids:
         dataset = api.dataset.get_info_by_id(dataset_id)
         images = api.image.get_list(dataset_id)
-
         for image_infos in sly.batched(images):
             image_ids = [image_info.id for image_info in image_infos]
             ann_infos = api.annotation.download_batch(dataset_id, image_ids)
-
             for image_info, ann_info in zip(image_infos, ann_infos):
                 ann_json = ann_info.annotation
                 ann = sly.Annotation.from_json(ann_json, meta)
@@ -72,21 +74,38 @@ def interactive_occurrence_matrix(api: sly.Api, task_id, context, state, app_log
                     counters[p].append((image_info, dataset))
             progress.iters_done_report(len(image_infos))
             fields = [
-                {"field": "data.progressCurrent", "payload": int(progress.current * 100 / progress.total)},
-                {"field": "data.progressTotal", "payload": project.items_count},
+                {"field": "data.progressCurrent", "payload": progress.current},
+                {"field": "data.progress", "payload": int(progress.current * 100 / progress.total)}
+
             ]
             api.app.set_fields(task_id, fields)
 
+    class_names = [cls.name for cls in meta.obj_classes]
+
+    #colors for pallete
+    min_value = None
+    max_value = None
+    for cls_name1 in class_names:
+        for cls_name2 in class_names:
+            key = frozenset([cls_name1, cls_name2])
+            imgs_cnt = len(counters[key])
+            min_value = imgs_cnt if min_value is None else min(min_value, imgs_cnt)
+            max_value = imgs_cnt if max_value is None else max(max_value, imgs_cnt)
+    norm = matplotlib.colors.Normalize(vmin=min_value, vmax=max_value)
+
+    # build finial table
     CELL_TO_IMAGES = defaultdict(lambda: defaultdict(list))
     pd_data = []
-    class_names = [cls.name for cls in meta.obj_classes]
     columns = ["name", *class_names]
     for cls_name1 in class_names:
         cur_row = [cls_name1]
         for cls_name2 in class_names:
             key = frozenset([cls_name1, cls_name2])
             imgs_cnt = len(counters[key])
-            cur_row.append(imgs_cnt)
+            rgba = cmap(norm(imgs_cnt), bytes=True)
+            hex = sly.color.rgb2hex(rgba[:3])
+            #cur_row.append(imgs_cnt)
+            cur_row.append(f'<div><i class="zmdi zmdi-stop mr5" style="color: {hex}"></i>{imgs_cnt}</div>')
 
             cell_images_data = []
             for (info, ds_info) in counters[key]:
@@ -120,6 +139,7 @@ def interactive_occurrence_matrix(api: sly.Api, task_id, context, state, app_log
     report_url = api.file.get_url(file_info.id)
 
     fields = [
+        {"field": "data.started", "payload": False},
         {"field": "data.table", "payload": {"columns": columns, "data": pd_data}},
         {"field": "data.cellToImages", "payload": CELL_TO_IMAGES},
         {"field": "data.savePath", "payload": remote_path},
@@ -142,14 +162,13 @@ def main():
         "projectId": "",
         "projectName": "",
         "projectPreviewUrl": "",
+        "progress": 0,
         "progressCurrent": 0,
         "progressTotal": 0,
         "clickedCell": "not clicked",
         "table": {"columns": [], "data": []},
-        "selectedRow": {},
-        "selectedColumnName": "",
+        "selection": {},
         "cellToImages": {"columns": [], "data": []},
-        "debugEvent": {}
     }
     state = {
     }
